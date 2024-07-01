63d15fe96414f14dee559600d18d6fb4bbe84915
Alfie <MaskellAlfie@outlook.com>
2024-07-01 15:44:55 +0100

    Animate Logo but don't do it properly!

--- js/layout.js
+++ js/layout.js
@@ -1,35 +1,15 @@
 const graphBars = document.querySelectorAll('.logo-graph-bar');
 
-// Growing Graph Bars on DOM Load
-document.addEventListener('DOMContentLoaded', function() {
+// Event Listeners Bars
+graphBars.forEach((bar) => {
+    bar.style.setProperty('--current-height', "1%"); 
+    bar.style.setProperty('--target-height', (Math.random() * 100).toFixed() + "%");
 
-    // Handling Animation
-    graphBars.forEach((bar) => {
-        const targetHeight = bar.getAttribute('data-height');
-
-        // Animation Code
-        bar.style.setProperty('--target-height', targetHeight);
-        bar.style.animation = "growBarFromStart 1s forwards";
-    });
-
-});
-
-
-// Graph Bar Containers
-const graphBarsContainers = document.querySelectorAll('.logo-graph-bar-container');
-
-// Event Listeners for Mouse Over on Bars
-graphBarsContainers.forEach((barContainer) => {
-    // Get Child of Container 
-    
-    barContainer.addEventListener('mouseover', function() {
-        bar = barContainer.querySelector('.logo-graph-bar');
-        bar.style.animation = "shrinkBar 1s forwards";
-    });
-
-    barContainer.addEventListener('mouseout', function() {
-        bar = barContainer.querySelector('.logo-graph-bar');
+    // Animation End
+    bar.addEventListener('animationiteration', function() {
+        console.log("setting new values");
         bar.style.setProperty('--current-height', getComputedStyle(bar).height);
-        bar.style.animation = "growBarFromCurrent 1s forwards";
+        bar.style.setProperty('--target-height', (Math.random() * 100).toFixed() + "%");
     });
+    
 });
